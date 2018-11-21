7f678ce9bdc4e95a90487a0c102c769584fa2aac
dewyoyo <dewyoyo@hotmail.com>
2018-11-20 17:04:58 -0700

    made funtion showimg

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,83 +1,83 @@
 var triviaList = [
     {
-        question : "Which Nickelodeon adventurer grew quickly from seven to ten years old, and moved to the city in a spin-off in 2014?", //question
-        examp1 : "Henry Danger",                 //examp1
-        examp2 : "Dora the Explorer",            //examp2
-        examp3 : "SpongeBob SquarePants",        //examp3
-        examp4 : "The Bureau of Magical Things",   //examp4
-        answer : 2,              //right answer
-        explanation : "The spin-off series is called Dora and Friends: Into the City.",   //explanation ,interesting information
-        image : "assets/images/giphy_dora.gif"
+        question: "Which Nickelodeon adventurer grew quickly from seven to ten years old, and moved to the city in a spin-off in 2014?", //question
+        examp1: "Henry Danger",                 //examp1
+        examp2: "Dora the Explorer",            //examp2
+        examp3: "SpongeBob SquarePants",        //examp3
+        examp4: "The Bureau of Magical Things",   //examp4
+        answer: 2,              //right answer
+        explanation: "The spin-off series is called Dora and Friends: Into the City.",   //explanation ,interesting information
+        image: "assets/images/giphy_dora.gif"
     },
     {
-        question : "What are Chubby Hubby, Half-Baked and Vermonty Python?",
-        examp1 : "Punk bands",
-        examp2 : "Comedians",
-        examp3 : "Cars",
-        examp4 : "Ice cream flavours",
-        answer : 4,
-        explanation : "They are just three of Ben & Jerry's ice cream flavours. Vermonty Python is coffee liqueur flavoured with cookie pieces and fudge. Half-Baked is a blend of chocolate and vanilla ice cream with gobs of cookie dough and pieces of brownie, and Chubby Hubby is vanilla ice cream with peanut-butter filled pretzel pieces and a ribbon of fudge.",
-        image : "assets/images/giphy_icecream.gif"
+        question: "What are Chubby Hubby, Half-Baked and Vermonty Python?",
+        examp1: "Punk bands",
+        examp2: "Comedians",
+        examp3: "Cars",
+        examp4: "Ice cream flavours",
+        answer: 4,
+        explanation: "They are just three of Ben & Jerry's ice cream flavours. Vermonty Python is coffee liqueur flavoured with cookie pieces and fudge. Half-Baked is a blend of chocolate and vanilla ice cream with gobs of cookie dough and pieces of brownie, and Chubby Hubby is vanilla ice cream with peanut-butter filled pretzel pieces and a ribbon of fudge.",
+        image: "assets/images/giphy_icecream.gif"
     },
     {
-        question : "Who is the pre-Civil War author of a short story about a beating heart beneath the floorboards?",
-        examp1 : "Roald Dahl",
-        examp2 : "Emily Dickinson",
-        examp3 : "Edgar Allan Poe",
-        examp4 : "Robert Frost",
-        answer : 3,
-        explanation : "The famous piece is The Tell-Tale Heart.",
-        image : "assets/images/EdgarAllanPoe.jpg"
+        question: "Who is the pre-Civil War author of a short story about a beating heart beneath the floorboards?",
+        examp1: "Roald Dahl",
+        examp2: "Emily Dickinson",
+        examp3: "Edgar Allan Poe",
+        examp4: "Robert Frost",
+        answer: 3,
+        explanation: "The famous piece is The Tell-Tale Heart.",
+        image: "assets/images/EdgarAllanPoe.jpg"
     },
     {
-        question : "Which school was Maya Lin attending when she designed the Vietnam Veterans Memorial as part of a class assignment?",
-        examp1 : "Yale University",
-        examp2 : "UC Berkeley",
-        examp3 : "Stanford University",
-        examp4 : "Massachusetts Institute of Technology (MIT)",
-        answer : 1,
-        explanation : "Initially scorned, the memorial is now cherised by millions of visitors each year.",
-        image : "assets/images/Yale_University.png"
+        question: "Which school was Maya Lin attending when she designed the Vietnam Veterans Memorial as part of a class assignment?",
+        examp1: "Yale University",
+        examp2: "UC Berkeley",
+        examp3: "Stanford University",
+        examp4: "Massachusetts Institute of Technology (MIT)",
+        answer: 1,
+        explanation: "Initially scorned, the memorial is now cherised by millions of visitors each year.",
+        image: "assets/images/Yale_University.png"
     },
     {
-        question : "What cattle end-product was named Wisconsin's Unofficial State Muffin in 1989?",
-        examp1 : "Jello",
-        examp2 : "Cow Chip",
-        examp3 : "Dish soap",
-        examp4 : "Candles",
-        answer : 2,
-        explanation : "If you're not sure what a cow chip is, just please don't try to eat this 'muffin'. A cow chip is a dried pile of cow dung that is used at cow-chip-throwing competitions. Chips are often tossed Frisbee-style. They can fly more than 150 feet!",
-        image : "assets/images/chow-chip.jpg"
+        question: "What cattle end-product was named Wisconsin's Unofficial State Muffin in 1989?",
+        examp1: "Jello",
+        examp2: "Cow Chip",
+        examp3: "Dish soap",
+        examp4: "Candles",
+        answer: 2,
+        explanation: "If you're not sure what a cow chip is, just please don't try to eat this 'muffin'. A cow chip is a dried pile of cow dung that is used at cow-chip-throwing competitions. Chips are often tossed Frisbee-style. They can fly more than 150 feet!",
+        image: "assets/images/chow-chip.jpg"
     },
     {
-        question : "Which patriotic Bostonian won the acquittal of most of the British troops involved in the 1770 Boston Massacre that killed five civilians?",
-        examp1 : "Samuel Adams",
-        examp2 : "James Otis",
-        examp3 : "John Adams",
-        examp4 : "John Hancock",
-        answer : 3,
-        explanation : "He strongly believed all people deserved a fair trial and a thorough defense.",
-        image : "assets/images/johnAdams.jfif"
+        question: "Which patriotic Bostonian won the acquittal of most of the British troops involved in the 1770 Boston Massacre that killed five civilians?",
+        examp1: "Samuel Adams",
+        examp2: "James Otis",
+        examp3: "John Adams",
+        examp4: "John Hancock",
+        answer: 3,
+        explanation: "He strongly believed all people deserved a fair trial and a thorough defense.",
+        image: "assets/images/johnAdams.jfif"
     },
     {
-        question : "Which U.S. state has the longest coastline?",
-        examp1 : "Delaware",
-        examp2 : "Georgia",
-        examp3 : "Kansas",
-        examp4 : "Alaska",
-        answer : 4,
-        explanation : "It has 6,640 miles of coastline.",
-        image : "assets/images/giphy-Alaska.gif"
+        question: "Which U.S. state has the longest coastline?",
+        examp1: "Delaware",
+        examp2: "Georgia",
+        examp3: "Kansas",
+        examp4: "Alaska",
+        answer: 4,
+        explanation: "It has 6,640 miles of coastline.",
+        image: "assets/images/giphy-Alaska.gif"
     },
     {
-        question : "What is'The Devil's Picture Book'?",
-        examp1 : "A deck of cards",
-        examp2 : "Marvel Comics",
-        examp3 : "A bathroom mirror",
-        examp4 : "Flat pack assembly instructions",
-        answer : 1,
-        explanation : "The first decks of cards, from which modern playing cards have descended, were Tarot cards which are still used by fortune tellers, and the soubriquet derives from the bible forbidding any form of divination. Comics can be great fun and I find mirrors quite useful (so long as the lighting is subdued) but I'm convinced that assembly instructions are compiled by using an internet translater to translate comprehensible English into Japanese, translating the results into Italian, then converting the Italian translation back into English so that it makes no sense at all!",
-        image : "assets/images/giphy-deckofcards.gif"
+        question: "What is'The Devil's Picture Book'?",
+        examp1: "A deck of cards",
+        examp2: "Marvel Comics",
+        examp3: "A bathroom mirror",
+        examp4: "Flat pack assembly instructions",
+        answer: 1,
+        explanation: "The first decks of cards, from which modern playing cards have descended, were Tarot cards which are still used by fortune tellers, and the soubriquet derives from the bible forbidding any form of divination. Comics can be great fun and I find mirrors quite useful (so long as the lighting is subdued) but I'm convinced that assembly instructions are compiled by using an internet translater to translate comprehensible English into Japanese, translating the results into Italian, then converting the Italian translation back into English so that it makes no sense at all!",
+        image: "assets/images/giphy-deckofcards.gif"
     }
 ];
 var shuffTriviaList = shuffle(triviaList);
@@ -127,13 +127,19 @@ function decrement(k) {
         $(".ques-div").html("<p style='color:black;font-size:18pt;font-weight:bold;'>Times Up!</p>");
         // $(".ques-div").append("The Correct Answer was " + anskey + ". <br>");
         //explanation and img
-        $(".ques-div").append(shuffTriviaList[k].explanation + "<br>");
-        $(".ques-div").append("<img id='trivia-img' src=" + shuffTriviaList[k].image + ">");
-        // console.log(".ques-div: " + $(".ques-div").text());
-        timerId = setTimeout(retriveQues, ansTime, (k + 1));
-        clearInterval(intervalId);
+        showImg(k);
     }
 }
+// show images and explanation
+function showImg(k) {
+    //explanation and img
+    $(".ques-div").append(shuffTriviaList[k].explanation + "<br>");
+    $(".ques-div").append("<img id='trivia-img' src=" + shuffTriviaList[k].image + ">");
+    // console.log(".ques-div: " + $(".ques-div").text());
+    timerId = setTimeout(retriveQues, ansTime, (k + 1));
+    clearInterval(intervalId);
+}
+
 function resetDiv() {
     $(".ques-div").empty();
     $(".exmp-div").empty();
@@ -152,12 +158,7 @@ function correctAns(k) {
     console.log("numOfCorrAns: " + numOfCorrAns);
     resetDiv();
     $(".ques-div").html("<p style='color:black;font-size:18pt;font-weight:bold;'>Correct</p>");
-    //explanation and img
-    $(".ques-div").append(shuffTriviaList[k].explanation + "<br>");
-    $(".ques-div").append("<img id='trivia-img' src=" + shuffTriviaList[k].image + ">");
-    console.log(".ques-div: " + $(".ques-div").text());
-    timerId = setTimeout(retriveQues, ansTime, (k + 1));
-    clearInterval(intervalId);
+
 };
 function wrongAns(anskey, k) {
     console.log("Wrong answer");
@@ -166,12 +167,7 @@ function wrongAns(anskey, k) {
     resetDiv();
     $(".ques-div").html("<p style='color:black;font-size:18pt;font-weight:bold;'>Nope</p>");
     $(".ques-div").append("The Correct Answer was " + anskey + ". <br>");
-    //explanation and img
-    $(".ques-div").append(shuffTriviaList[k].explanation + "<br>");
-    $(".ques-div").append("<img id='trivia-img' src=" + shuffTriviaList[k].image + ">");
-    // console.log(".ques-div: " + $(".ques-div").text());
-    timerId = setTimeout(retriveQues, ansTime, (k + 1));
-    clearInterval(intervalId);
+
 };
 
 // display the trivia question and examples.
@@ -221,6 +217,8 @@ function retriveQues(i) {
             else {
                 wrongAns(shuffTriviaList[i].answer, i);
             };
+
+            showImg(i);
         });
     };
 };
